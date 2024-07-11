5b7f74b17d5e55b488ff73f5250532bb90c240df
Kemialabede <60380985+Kemialabede@users.noreply.github.com>
2024-07-11 16:28:45 +0100

    updated pdf size

--- src/components/pdfPreviewer/viewPdf.jsx
+++ src/components/pdfPreviewer/viewPdf.jsx
@@ -4,7 +4,7 @@ import Modal from '../modal'
 const ViewPdf = ({ isShown, setIsShown, children }) => {
   return (
      <div className='view_pdf'>
-    <Modal isShown={isShown} width={900} setIsShown={setIsShown} title="Mathematics 01">
+    <Modal isShown={isShown} width={750} setIsShown={setIsShown} title="Mathematics 01">
         {children}
     </Modal>
     </div>
